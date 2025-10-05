2e4b611f7bdda8abe010c44d024d685dcaa9b5a7
Vivek Kumar <vivek142000kumar@gamil.com>
2025-10-05 17:56:11 +0530

    port

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.use("/api/enquiries", enquiryRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/warranty", warrantyRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
